4f8428026bc83eb426e5724ae0cc55954f2f3f27
Dispray <174300360+Dispray@user.noreply.github.com>
2024-08-05 11:03:48 +0800

    Add function getComputerChoice()

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('MAMA MIA')
+function getComputerChoice(){
+    let choice = "";
+    let randomChoice = Math.random();
+
+    if (randomChoice>0.33){
+        choice="rock"
+    } 
+    else if (randomChoice<0.67){
+        choice="paper"
+    }
+    else{
+        choice="scissors"
+    }
+
+    return choice
+}
+
+console.log(getComputerChoice)
+console.log("rat")
+console.log(Math.random())
