28106e5929c88aa8915ad3be13ce9a54f0192b69
Dispray <174300360+Dispray@user.noreply.github.com>
2024-08-05 11:07:06 +0800

    Fix wrong output from getComputerChoice function

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ function getComputerChoice(){
     let choice = "";
     let randomChoice = Math.random();
 
-    if (randomChoice>0.33){
+    if (randomChoice<0.33){
         choice="rock"
     } 
-    else if (randomChoice<0.67){
-        choice="paper"
-    }
-    else{
+    else if (randomChoice>0.67){
         choice="scissors"
     }
-
+    else{
+        choice="paper"
+    }
+    console.log(randomChoice)
     return choice
 }
 
-console.log(getComputerChoice)
-console.log("rat")
-console.log(Math.random())
+let dog = getComputerChoice()
+console.log(dog)
